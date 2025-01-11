333d7d11dc2f081ce0f4a8ffbf67271ef66c9f98
Adnibog <govindapandey223@gmail.com>
2025-01-11 04:09:30 -0500

    Updated portfolio website with new content

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Navbar.css'; // Assuming you will create a CSS file for styling
+import './Navbar.css';
 
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <div className="navbar-brand">
-                <h1>Gobinda Pandey</h1>
-                <p>PhD Student in Computing and Information Sciences</p>
-            </div>
-            <ul className="navbar-links">
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/research-interests">Research Interests</Link></li>
-                <li><Link to="/publications">Publications</Link></li>
-                <li><Link to="/projects">Projects</Link></li>
-                <li><Link to="/certifications">Certifications</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
-            </ul>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/research-interests">Research Interests</Link>
+            <Link to="/publications">Publications</Link>
+            <Link to="/certifications">Certifications</Link>
+            <Link to="/projects">Projects</Link>
+            <Link to="/contact">Contact</Link>
         </nav>
     );
 };
