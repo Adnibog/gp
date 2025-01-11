9205d5fa6069d860c88a3d5a1d427717f9be4e20
Adnibog <govindapandey223@gmail.com>
2025-01-11 17:08:57 -0500

    Fix image paths and HTML structure for GitHub Pages

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,17 +5,17 @@ const projects = [
     {
         title: 'Trek Monitoring System',
         description: 'Built a prototype gadget that updates trekkers about current location, humidity, weather forecast, and basic health information like body temperature, and heart rate. Published as a paper in the Journal of Electronics and Informatics.',
-        image: `${process.env.PUBLIC_URL}/images/trek.png`
+        image: `${process.env.PUBLIC_URL}/trek.png`
     },
     {
         title: 'RFID-based Electronic Health Record Management System',
         description: 'Developed an RFID-based system to securely record and access electronic health records. Published as a paper in the Journal of Information Technology and Digital World.',
-        image: `${process.env.PUBLIC_URL}/images/smart.jpg`
+        image: `${process.env.PUBLIC_URL}/smart.jpg`
     },
     {
         title: 'Agrobot: Multi-Function Agricultural Robot',
         description: 'Prototyped a remote controller-based machine to plough fields, sow seeds, and water plants.',
-        image: `${process.env.PUBLIC_URL}/images/agro.jpeg`
+        image: `${process.env.PUBLIC_URL}/agro.jpeg`
     }
 ];
 
