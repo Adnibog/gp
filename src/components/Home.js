e59ecd73cd06dc1931ab12d5421f02ae44a7e2d7
Adnibog <govindapandey223@gmail.com>
2025-01-11 23:49:34 -0500

    Update background color, fix CV link, and ensure full responsiveness

--- src/components/Home.js
+++ src/components/Home.js
@@ -25,10 +25,9 @@ const Home = () => {
                     <p>Exploring Secure Technologies for a Better Tomorrow.</p>
                     <p>With a strong background in electronics, communication, and information, and a passion for cybersecurity, I am dedicated to advancing the field through innovative research and practical applications.</p>
                     <a href={`${process.env.PUBLIC_URL}/Adnibog_CV.pdf`} className="cv-link" target="_blank" rel="noopener noreferrer">View CV</a>
-                    <iframe src={`${process.env.PUBLIC_URL}/Adnibog_CV.pdf`} className="cv-iframe" title="CV" style={{ width: '100%', height: '500px', border: 'none' }}></iframe>
                 </div>
             </section>
-            <section className="skills section" style={{ backgroundColor: 'lightgreen' }}>
+            <section className="skills section">
                 <div className="hero-content">
                     <h2>Skills</h2>
                     <div className="skills-grid">
