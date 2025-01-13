c247d0a6e1ee8e623810c906bd50417b97c97f60
Adnibog <govindapandey223@gmail.com>
2025-01-13 14:54:43 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/gp/precache-manifest.6a4c2d62bd788a687b644a2c203f8f2f.js"
+  "/gp/precache-manifest.c26c5e975f4181f9f434c5fe4dacaf62.js"
 );
 
 self.addEventListener('message', (event) => {
