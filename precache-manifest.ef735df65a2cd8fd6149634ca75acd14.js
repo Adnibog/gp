505ed2601827206765153394d2e6b5ffa39a7dc5
Adnibog <govindapandey223@gmail.com>
2025-01-14 17:49:50 -0500

    Updates

--- precache-manifest.ef735df65a2cd8fd6149634ca75acd14.js
+++ precache-manifest.ef735df65a2cd8fd6149634ca75acd14.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2e838882d0442321083b17d048800072",
+    "revision": "5fb30ace8a3b00856e64694d4eead46c",
     "url": "/gp/index.html"
   },
   {
-    "revision": "6b79920e648b40560a39",
-    "url": "/gp/static/css/main.6175fc99.chunk.css"
+    "revision": "1dd434bd8b235844d1b2",
+    "url": "/gp/static/css/main.329033ea.chunk.css"
   },
   {
     "revision": "01ea97c0a12b2663859c",
     "url": "/gp/static/js/2.81fd6637.chunk.js"
   },
   {
-    "revision": "6b79920e648b40560a39",
+    "revision": "1dd434bd8b235844d1b2",
     "url": "/gp/static/js/main.c4abdf30.chunk.js"
   },
   {
