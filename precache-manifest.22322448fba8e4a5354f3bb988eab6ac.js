4c1471988b04c905cf5fc9bb663e015f21f17fb5
Adnibog <govindapandey223@gmail.com>
2025-04-27 15:19:07 -0400

    Updates

---
+++ precache-manifest.22322448fba8e4a5354f3bb988eab6ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7b5dc92459503e5abdd0ef98025137e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9a72ca8c1ab4cb26aea",
+    "url": "/static/css/main.403a4018.chunk.css"
+  },
+  {
+    "revision": "1682b56e36d5d79ed899",
+    "url": "/static/js/2.a557fc6e.chunk.js"
+  },
+  {
+    "revision": "ceaea57332387e7b8fc3e378a4204743",
+    "url": "/static/js/2.a557fc6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9a72ca8c1ab4cb26aea",
+    "url": "/static/js/main.ba0d3a8e.chunk.js"
+  },
+  {
+    "revision": "430f72daf7c3a4d42642",
+    "url": "/static/js/runtime-main.351e9c47.js"
+  }
+]);
