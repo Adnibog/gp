964107f0dbb8fa70380ec2515b8972587d479048
Adnibog <govindapandey223@gmail.com>
2025-06-23 05:07:16 -0600

    Updates

---
+++ precache-manifest.37de706fcb883c041036d9eeb82ce238.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c11d0e1e4adff558b7eb747afcc21af6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5490a5b735c7810b831",
+    "url": "/static/css/main.b1497a1d.chunk.css"
+  },
+  {
+    "revision": "a82ff9d13e7d3c04cabe",
+    "url": "/static/js/2.95403b8d.chunk.js"
+  },
+  {
+    "revision": "9ffd63ab6d5f6514aadd1f4315b6c7c7",
+    "url": "/static/js/2.95403b8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5490a5b735c7810b831",
+    "url": "/static/js/main.f8b34ed4.chunk.js"
+  },
+  {
+    "revision": "430f72daf7c3a4d42642",
+    "url": "/static/js/runtime-main.351e9c47.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.81ce5f9f51eaacb9c6834e7a29d18e56.js"
+  "/precache-manifest.37de706fcb883c041036d9eeb82ce238.js"
 );
 
 self.addEventListener('message', (event) => {
